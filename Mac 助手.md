fb2e35d250ccf91a38f130b98386470d839fc5d3
pamela.huang <pamela.huang@vantopgroup.com>
2020-12-11 09:30:01 +0800

    工作资料转移

---
+++ Mac 助手.md
@@ -0,0 +1,21 @@
+# Mac 助手
+
+您身边的mac专家
+
+
+
+- 开发环境
+
+  - [homebrew](https://brew.sh/) 安装工具
+
+    安装git前先安装这个
+
+  - [git](https://gist.github.com/derhuerst/1b15ff4652a867391f03)`brew install git`
+
+- 工具
+
+  - [Iterm2](https://iterm2.com/) 终端
+  - [oh-my-zsh](https://ohmyz.sh/) 终端
+  - [autojump](https://github.com/wting/autojump) 终端
+  - [typora](https://typora.io/) md工具
+
---
+++ 需求文档/BI需求/原型/files/分析/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,_(g,h,i,_(j,k,l,k)),m,[],n,_(h,o),p,[q,r,s,t,u],v,_(w,x,y,z,g,A,B,_(),C,[],D,_(E,F,G,H,I,_(J,K,L,M),N,null,O,P,P,Q,R,S,null,T,U,V,W,X,Y,Z,U,ba,bb,_(J,K,L,bc),bd,U,be,bf,_(bg,bh,bi,bj,bk,bj,bl,bj,bm,k,L,_(bn,bo,bp,bo,bq,bo,br,bs)),i,_(j,k,l,k)),bt,_(),bu,_(),bv,_(bw,[])),bx,_(),by,_());}; 
+var b="url",c="分析.html",d="generationDate",e=new Date(1597130875347.64),f="defaultAdaptiveView",g="name",h="",i="size",j="width",k=0,l="height",m="adaptiveViews",n="sketchKeys",o="s0",p="variables",q="OnLoadVariable",r="currentTime",s="NewVariable",t="NewVariable1",u="NewVariable2",v="page",w="packageId",x="435e8b2ea7324ff3b45b53385bf517c5",y="type",z="Axure:Page",A="分析",B="notes",C="annotations",D="style",E="baseStyle",F="627587b6038d43cca051c114ac41ad32",G="pageAlignment",H="center",I="fill",J="fillType",K="solid",L="color",M=0xFFFFFFFF,N="image",O="imageAlignment",P="near",Q="imageRepeat",R="auto",S="favicon",T="sketchFactor",U="0",V="colorStyle",W="appliedColor",X="fontName",Y="Applied Font",Z="borderWidth",ba="borderVisibility",bb="borderFill",bc=0xFF797979,bd="cornerRadius",be="cornerVisibility",bf="outerShadow",bg="on",bh=false,bi="offsetX",bj=5,bk="offsetY",bl="blurRadius",bm="spread",bn="r",bo=0,bp="g",bq="b",br="a",bs=0.349019607843137,bt="adaptiveStyles",bu="interactionMap",bv="diagram",bw="objects",bx="masters",by="objectPaths";
+return _creator();
+})());
